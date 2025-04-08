cac5ba8892dfd82a26132fd658132f4918928101
Daniel Lisboa <daniel.lisboa.alves@gmail.com>
2025-04-08 09:08:01 -0300

    Implementando o Adapter no código

--- app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
+++ app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
@@ -1,20 +1,47 @@
 package com.devspace.recyclerview
 
 import android.provider.ContactsContract.Contacts
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+
 //Adaptação entre o data class e o item_list layout
 class ContactListAdapter :
     ListAdapter<Contact, ContactListAdapter.ContactViewHolder>(ContactDiffUtils()) {
 
+    //Criar um ViewHolder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+        return ContactViewHolder(view)
+    }
 
-        //view holder = view que segura os dados
+    //Atrelar o dado com a UI
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val contact = getItem(position)
+        holder.bind(contact)
+    }
+
+
+    //view holder = view que segura os dados
     class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val tvName = view.findViewById<TextView>(R.id.tv_name)
+        private val tvPhone = view.findViewById<TextView>(R.id.tv_phone)
+        private val image = view.findViewById<ImageView>(R.id.imageView)
+        fun bind(contact: Contact) {
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
+            image.setImageResource(contact.icon)
+
+        }
 
     }
-        //compara a diferença quando a lista é atualizada
+
+    //compara a diferença quando a lista é atualizada
     class ContactDiffUtils : DiffUtil.ItemCallback<Contact>() {
         override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
             return oldItem == newItem
